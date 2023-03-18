e9d88c63bb0c9ac166f30d3af688ecb8419db822
bhavnish97 <arorahavnish108@gmail.com>
2023-03-18 21:00:39 +0530

    Model Training

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -22,9 +22,9 @@ from src.components.model_trainer import ModelTrainer, ModelTrainerConfig
 
 @dataclass
 class DataIngestionConfig:
-    train_data_path: str = os.path.join('artifact',"train.csv")
-    test_data_path: str = os.path.join('artifact',"test.csv")
-    raw_data_path: str = os.path.join('artifact',"raw.csv")
+    train_data_path: str = os.path.join('artifacts',"train.csv")
+    test_data_path: str = os.path.join('artifacts',"test.csv")
+    raw_data_path: str = os.path.join('artifacts',"raw.csv")
 
 
 class DataIngestion:
